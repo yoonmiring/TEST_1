7ee5c5de3a3feeffe14544a0c6655b691fb7c2f3
jeongyoonmin <rapaella1109@gmail.com>
2022-03-16 13:42:40 +0900

    220316 롯데월드.cpp 만나이값까지 출력가능

--- source/220316 롯데월드.cpp
+++ source/220316 롯데월드.cpp
@@ -1,24 +1,86 @@
 #include <stdio.h>
 
 int main(){
-	int choose,ticket,number,orderCount,discount,price;
-	const int  BABY_PRICE =15000, ALL_DAY_ADULT_PRICE = 59000,ALL_AFTER_ADULT_PRICE = 48000, ALL_DAY_TEEN_PRICE = 52000, ALL_AFTER_TEEN_PRICE = 42000, ALL_DAY_CHILD_PRICE = 47000,ALL_AFTER_CHILD_PRICE = 36000;//종합가격 
+	int choose,ticket,FrontNumber,BackNumber,orderCount,discount,price;
+	const int  ALL_DAY_ADULT_PRICE = 59000,ALL_AFTER_ADULT_PRICE = 48000, ALL_DAY_TEEN_PRICE = 52000, ALL_AFTER_TEEN_PRICE = 42000, ALL_DAY_CHILD_PRICE = 47000,ALL_AFTER_CHILD_PRICE = 36000;//종합가격 
 	const int  PARK_DAY_ADULT_PRICE = 56000,PARK_AFTER_ADULT_PRICE = 45000, PARK_DAY_TEEN_PRICE = 50000, PARK_AFTER_TEEN_PRICE = 40000, PARK_DAY_CHILD_PRICE = 46000,PARK_AFTER_CHILD_PRICE = 35000;//파크 가격
+	const int  BABY_PRICE =15000, ADULT_PRICE = 48000, TEEN_PRICE = 42000, CHILD_PRICE = 36000;// 기본가격
+ 
+	int numberYear,birth,age,oldage,newage; 
 	 
-	
 	printf("선택해주세요.\n1. 종합이용권(롯데월드+민속박물관)  \n2. 파크이용권\n");
 	scanf("%d",&choose);
 	printf("권종을 선택하세요.\n1. 1DAY \n2. AFTER4(오후 4시 이후 입장)\n");
 	scanf("%d",&ticket);
-	printf("주민번호를 입력하세요.\n");
-	scanf("%d",&number);
+	printf("주민번호 앞자리 6자글자와 뒷자리 1자리를 입력하세요.\n 앞자리(6글자)\n");
+	scanf("%d",&FrontNumber);
+	printf("뒷자리(1글자)\n");
+	scanf("%d",&BackNumber);	
 	printf("몇개를 주문하시겠습니가?(최대 10개)\n");
 	scanf("%d",&orderCount);
-	printf("우대사항을 선택하세요.\n1. 없음(나이 우대는 자동처리)\n2. 장애인\n3. 국가유공자\n4. 다자녀\n5.임산부\n");
+	printf("우대사항을 선택하세요.\n1. 없음(나이 우대는 자동처리)\n2. 장애인\n3. 국가유공자\n4. 다둥이 행복\n5.임산부\n6.휴가장병 \n");
 	scanf("%d",&discount);
 
+		
+		numberYear =FrontNumber/10000; //생년 출력
+		birth= FrontNumber-(numberYear * 10000); // 생일 출력
+		 if (BackNumber == 1 || BackNumber == 2){ // 1900년생 || 2000년생 출력 
+		 	oldage=numberYear+1900;
+		 } else if (BackNumber == 3 || BackNumber == 4){
+		 	newage=numberYear+2000;
+		 }
+		if( birth > 316 ){  //만나이
+		 
+			age= 2022 - oldage -1;
+			}else if (birth > 316){
+			age = 2022 -newage-1;
+			}else if (birth < 316){
+			age = 2022 -oldage;
+			}else if (birth < 316){
+			age = 2022 -newage;
+			}
+		
+		if ( age <=3 ){
+			BABY_PRICE;
+		}	else if ( age >3 && age <= 12){
+			CHILD_PRICE;
+		}	else if ( age >12 && age <= 18){
+			TEEN_PRICE;
+		}	else if ( age >19 && age <= 64){
+			ADULT_PRICE;
+		}	else if ( age <= 65){
+			CHILD_PRICE;
+		}
+				
+			
+		switch(choose){
+			case 1 :
+				switch(ticket){
+				case 1:
+				
 
-	printf("가격은 %d원 입니다.",price);
+				break;
+				case 2:
+				break;
+			}
+				
+				break;
+			case 2 :
+				switch(ticket){
+				case 1:
+				break;
+				case 2:
+				break;
+				}
+				break;
+			default:
+				printf("잘 못 입력하셨습니다. ");
+	}
+		
+
+
+
+	printf("가격은 %d원 입니다.\n감사합니다.",price);
 	
 	
 	

5cb65f5e1f45fa19e12f4965eeba92e7cd006137
jeongyoonmin <rapaella1109@gmail.com>
2022-03-16 11:32:08 +0900

    220316 롯데월드.cpp 상수값으로 가격 입력

--- source/220316 롯데월드.cpp
+++ source/220316 롯데월드.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	int choose,ticket,number,orderCount,discount,cost;
-
-	printf("선택해주세요.\n1. 종합이용권  \n2. 파크이용권\n");
+	int choose,ticket,number,orderCount,discount,price;
+	const int  BABY_PRICE =15000, ALL_DAY_ADULT_PRICE = 59000,ALL_AFTER_ADULT_PRICE = 48000, ALL_DAY_TEEN_PRICE = 52000, ALL_AFTER_TEEN_PRICE = 42000, ALL_DAY_CHILD_PRICE = 47000,ALL_AFTER_CHILD_PRICE = 36000;//종합가격 
+	const int  PARK_DAY_ADULT_PRICE = 56000,PARK_AFTER_ADULT_PRICE = 45000, PARK_DAY_TEEN_PRICE = 50000, PARK_AFTER_TEEN_PRICE = 40000, PARK_DAY_CHILD_PRICE = 46000,PARK_AFTER_CHILD_PRICE = 35000;//파크 가격
+	 
+	
+	printf("선택해주세요.\n1. 종합이용권(롯데월드+민속박물관)  \n2. 파크이용권\n");
 	scanf("%d",&choose);
-	printf("권종을 선택하세요.\n1. 1DAY \n2. AFTER\n");
+	printf("권종을 선택하세요.\n1. 1DAY \n2. AFTER4(오후 4시 이후 입장)\n");
 	scanf("%d",&ticket);
 	printf("주민번호를 입력하세요.\n");
 	scanf("%d",&number);
@@ -15,7 +18,7 @@ int main(){
 	scanf("%d",&discount);
 
 
-	printf("가격은 %d원 입니다.",cost) ;
+	printf("가격은 %d원 입니다.",price);
 	
 	
 	
